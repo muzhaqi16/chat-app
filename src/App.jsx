81648bdcf37cd61dd01a0da9a6ffce7599e2206b
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-03-05 10:39:23 -0500

    add react-router-dom and implement basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 
+//using react router to handle the routing for the application
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+
+import Nav from './components/Nav';
+
 const App = () => {
 	return (
-		<div>
-			<h1>Chat Application</h1>
-		</div>
+		//in order to use Link from react router we need to wrap it in <Router> tags
+		<Router>
+			<header>
+				<h1>Chat Application</h1>
+				<Nav />
+			</header>
+			<main>
+				{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+				<Switch>
+					<Route path="/login">
+						<h2>Log in</h2>
+					</Route>
+					<Route path="/signup">
+						<h2>Sign Up</h2>
+					</Route>
+					<Route path="/">
+						<h2>Main Chat</h2>
+					</Route>
+				</Switch>
+			</main>
+		</Router>
 	);
 };
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/signup">Sign Up</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
